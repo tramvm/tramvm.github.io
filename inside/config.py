380d4c0b3fa188aa4602408ba699a14a015600d3
TramVM <tramvm@gmail.com>
2019-08-04 22:37:57 +0700

    Update price

--- inside/config.py
+++ inside/config.py
@@ -321,7 +321,7 @@ class DefaultConfig(BaseConfig):
             'listed': True,
             'title': 'SSMMS auto captcha fastly | Online sand booking auto filling info with Tampermonkey script Greasemonkey script',
             'short_title': 'SSMMS auto captcha filling',
-            'price': 19,
+            'price': 100,
             'hit_donate': 100,
             'total_donate': 15,
             'slug_title': 'ssmms-auto-captcha-online-sand-booking-auto-fill-info',
@@ -340,10 +340,9 @@ class DefaultConfig(BaseConfig):
                            '</ul>'
                            '<p>No need to install any additional software.</p>'
                            '<p>With above features:</p>'
-                           '<ul><li>$19 for unlimited usage on 2 instance browsers.</li>'
-                           '<li>$45 for unlimited usage up to 8 browsers.</li>'
-                           '<li>$89 for unlimited usage up to 20 browsers.</li>'
-                           '<li>$149 for Up to 50 browsers.</li>'
+                           '<li>$99 for unlimited usage up to 10 browsers.</li>'
+                           '<li>$179 for unlimited usage up to 20 browsers.</li>'
+                           '<li>$230 for Up to 30 browsers.</li>'
                            '</ul>'
                            '<p>NO limit number of requests per day. NO limit using day.</p>',
             'info': [
